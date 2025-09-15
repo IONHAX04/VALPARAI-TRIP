1b4a714fff5c0e84cce7f5e631c47a16a89a2158
Thirukumara Dhanaseharan <mailtothirukumara@gmail.com>
2025-09-15 11:22:35 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBGPaM4JPH2ABllIdMyYHT6VlGgVCXFiz4",
-  authDomain: "studio-6728859378-c030c.firebaseapp.com",
-  databaseURL: "https://studio-6728859378-c030c-default-rtdb.firebaseio.com",
-  projectId: "studio-6728859378-c030c",
-  storageBucket: "studio-6728859378-c030c.firebasestorage.app",
-  messagingSenderId: "527931251156",
-  appId: "1:527931251156:web:face7e7a2b95b8a959de41"
+  apiKey: "AIzaSyAL__K3ia1s7u1PIP03CDuJ-D3YJjz7LjU",
+  authDomain: "studio-7228655129-b284f.firebaseapp.com",
+  databaseURL: "https://studio-7228655129-b284f-default-rtdb.firebaseio.com",
+  projectId: "studio-7228655129-b284f",
+  storageBucket: "studio-7228655129-b284f.firebasestorage.app",
+  messagingSenderId: "183525318020",
+  appId: "1:183525318020:web:82c30bafa4c730bdc991d1"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
